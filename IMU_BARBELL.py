e2d4842ae7431b6dab2c0c215fd6e61f64dcd8bb
Jacob Howenstein <jacob.howenstein@gmail.com>
2017-07-27 12:44:43 -0500

    updated serial connection script to python 3

--- IMU_BARBELL.py
+++ IMU_BARBELL.py
@@ -5,39 +5,41 @@ import sys
 
 def signal_handler(signal, frame):
         ser.close()
-        ser2.close()
         sys.exit(0)
 signal.signal(signal.SIGINT, signal_handler)
 
+port_name = # insert serial port name here (string format)
 
-ser = serial.Serial('/dev/cu.AdafruitEZ-Linka7b1-SPP', 57600, timeout=1)
+ser = serial.Serial(port_name, 57600, timeout=1)
+
 time.sleep(.1)
 
-print "Connection Successful"
+print("Connection Successful")
 
-filename = raw_input("Filename?: ")
+filename = input("Filename?: ")
 
 filename1 = filename + ".csv"
 
 text_file1 = open(filename1, 'w')
 
+
 while True:
 	if ser.inWaiting():
-		line = ser.readline()
-		print line.strip()
+		line = ser.readline().decode('utf-8')
+		print(line.strip())
 		if line.strip() == 'start':
-			ser.write('start')
+			ser.write(b'start')
 			break
 		else:
 			pass
 
 while True:
 	if ser.inWaiting():
-		line = ser.readline()
+		line = ser.readline().decode('utf-8')
 		if line.strip() == 'stop':
-			ser.write('stop')
+			ser.write(b'stop')
 			break
-		print line.strip()
+		print(line.strip())
 		text_file1.write(line)
 
 		
